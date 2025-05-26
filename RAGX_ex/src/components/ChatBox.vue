f3e69dea2c3a49b9f94299c2be0cfb80850f9559
Hyonii <131145818+Jeonghyun-An@users.noreply.github.com>
2025-05-26 13:41:01 +0900

    [design] Fix the UI

--- RAGX_ex/src/components/ChatBox.vue
+++ RAGX_ex/src/components/ChatBox.vue
@@ -31,7 +31,7 @@ const streamedText = ref("");
 const sendMessage = async () => {
     streamedText.value = "";
 
-    const response = await fetch("http://localhost:3000/chat", {
+    const response = await fetch("http://localhost:3002/chat", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
